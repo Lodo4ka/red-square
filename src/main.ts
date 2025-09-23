8625bee60c995c326ed5b6b7c0fd2e3ca098e845
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-09-23 15:44:42 +0300

    Update Docker and Nginx configurations to standardize port usage across services, and enhance CORS settings in main application to allow multiple origins and specify allowed methods and headers.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  if (process.env.NODE_ENV !== 'production') {
-    app.enableCors({
-      origin: ['http://localhost:5173'],
-      credentials: true,
-    });
-  }
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(Number(process.env.PORT) || 3000, '0.0.0.0');
 }
 bootstrap();
